ed11b4a52406637189974f93bf1fd09bca37a8e6
Tra Anh Thơ <thotra081105@gmail.com>
2024-11-25 22:49:21 +0700

    interface day2

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+     <!-- Font Awesome -->
+     <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="path/to/font-awesome/css/font-awesome.min.css"
+    />
     <!-- Bootstrap CSS -->
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -13,7 +22,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <title>FlashCard</title>
-    <link style="height: 50px; width: 50px;" rel="icon" type="image/x-icon" href="{{ asset('imgs/logo.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('imgs/logo.ico') }}" />
 
 </head>
 <body style="background-color: cadetblue;">
@@ -89,14 +98,17 @@
                 </div>
 
                 <!-- The slideshow/carousel -->
-                <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="la.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+                <div class="carousel-inner flex-row">
+                <div class="caro-item active">
+                    <div class="card-item flex-column">
+                        <h3>Los Angeles</h3>
+                        <img src="la.jpg" alt="Los Angeles" class="d-block img-card" style="width:100%">
+                    </div>
                 </div>
-                <div class="carousel-item">
+                <div class="caro-item">
                     <img src="chicago.jpg" alt="Chicago" class="d-block" style="width:100%">
                 </div>
-                <div class="carousel-item">
+                <div class="caro-item">
                     <img src="ny.jpg" alt="New York" class="d-block" style="width:100%">
                 </div>
                 </div>
@@ -114,8 +126,91 @@
            </div>
 
 
+         <!-- FOOTER -->
+         <footer id="footer">
+        <div class="footer--background container">
+          <div class="gird d-flex flex-column footerbox">
+            <div class="footer_box footer_box--1 d-flex flex-row">
+              <div class="footer_box--1--content">
+                <span class=""
+                  ><img class="logo" src="/assets/img/magiccode.png" alt=""
+                /></span>
+                <p class="fontw">
+                  Chúng tôi không ở đây để bán cho bạn
+
+                  <br />
+                  sản phẩm, chúng tôi bán giá trị thông qua<br />
+                  chuyên môn của chúng tôi.
+                </p>
+                <div class="footer_icon flex-row">
+                  <i class="icon_main icon_space fa-brands fa-facebook"></i
+                  ><i
+                    class="icon_main icon_space fa-brands fa-square-twitter"
+                  ></i
+                  ><i class="icon_main icon_space fa-brands fa-instagram"></i
+                  ><i class="icon_main icon_space fa-brands fa-linkedin-in"></i>
+                </div>
+              </div>
+              <div class="footer_box--1--content"></div>
+              <div class="footer_box--1--content">
+                <p class="fontw">
+                  <b>Địa Chỉ:</b> <br />470 Đường Trần Đại Nghĩa, Ngũ
+                  Hành Sơn, Đà Nẵng.
+                </p>
+                <p class="fontw"><b>Phone:</b><br />(84+)093472686999</p>
+                <p class="fontw"><b>Email:</b> <br />flashcard@company.com</p>
+              </div>
+              <div class="footer_box--1--content">
+                <p class="fontw">
+                  <b>Công Ty:</b> <br />Liên Hệ <br />Tính Năng <br />Giá Cả
+                </p>
+
+                <p></p>
+              </div>
+            </div>
+            <div
+              class="footer_box footer_box--2 gird--center gird--center--column"
+            >
+              <p class="text" style="color: white">
+                Đăng ký để nhận thông tin cập nhật mới nhất.
+              </p>
+              <div class="email_button gird gird--center gird--center--row">
+                <input
+                  type="text"
+                  value=""
+                  placeholder="Nhập Địa Chỉ Email Của Bạn"
+                  style="color: rgb(219, 112, 147); width: 50%"
+                />
+                <button
+                  style="
+                    background-color: rgb(219, 112, 147);
+                    color: --color_white;
+                    border: 0;
+                    width: 20%;
+                  "
+                >
+                  Đăng Kí
+                </button>
+              </div>
+            </div>
+            <div class="footer_box footer_box--3">
+              <p class="fontw">
+                Được Hỗ Trợ Bởi
+                <span style="color: rgb(219, 112, 147)"> &copy; TraAnhTho</span>
+                &nbsp;
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
 
     </div>
-    
+
+
+
+
+    <!-- <div class="box">
+
+    </div> -->
 </body>
 </html>
