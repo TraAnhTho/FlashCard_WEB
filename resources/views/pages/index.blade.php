67c1c447c301cccd7ef451d99a90b6bc4f9c9776
Tra Anh Thơ <thotra081105@gmail.com>
2024-11-26 01:04:42 +0700

    interface day3

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -25,12 +25,12 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('imgs/logo.ico') }}" />
 
 </head>
-<body style="background-color: cadetblue;">
+<body style="background-color: #f6f7fb;">
     <div>
         <!-- HEADER -->
          <header class="header">
             <div class="container-fluid">
-                <ul class="nav">
+                <ul class="nav flex-row">
                     <li class="nav-item logo-item">
                         <img class="logo-item" src="{{ asset('imgs/logoflashcard_blue.png') }}" alt="">
                     </li>
@@ -69,14 +69,23 @@
                         </div> -->
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link disabled" href="#">Công cụ học</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                        <a class="nav-link disabled" href="#">Chủ đề</a>
+                    </li>
+                    <li class="nav-item" style="width: 40%;">
+                        <input type="text"  placeholder="Tìm kiếm" />
+                    </li>
+                    <li class="nav-item">
+                        <button class="">Đăng Kí</button>
+                    </li>
+                    <li class="nav-item">
+                        <button>Đăng Kí</button>
                     </li>
                 </ul>
             </div>
-            <div class="container">
+            <div class="title-contain container">
                 <h1>Bạn muốn học như thế nào? </h1>
                 <p>Nắm vững kiến thức đang học với thẻ ghi nhớ tương tác, bài kiểm tra thử và hoạt động học tập của Quizlet.</p>
                 <button>Đăng Kí Miễn Phí</button>
@@ -88,41 +97,103 @@
            <div class="container-fluid main-content">
             <div class="container-fluid content-part--one">
                 <!-- Carousel -->
-                <div id="demo" class="carousel slide" data-bs-ride="carousel">
+                <!-- <div id="demo" class="carousel slide" data-bs-ride="carousel"> -->
 
                 <!-- Indicators/dots -->
-                <div class="carousel-indicators">
+                <!-- <div class="carousel-indicators">
                 <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
                 <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
                 <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-                </div>
+                </div> -->
 
                 <!-- The slideshow/carousel -->
-                <div class="carousel-inner flex-row">
+                <div class="caro-inner flex-row">
                 <div class="caro-item active">
                     <div class="card-item flex-column">
-                        <h3>Los Angeles</h3>
-                        <img src="la.jpg" alt="Los Angeles" class="d-block img-card" style="width:100%">
+                        <h3>Học</h3>
+                        <img src="{{ asset('imgs/partone-0.avif') }}" alt="Los Angeles" class="d-block img-card" style="width:100%">
                     </div>
                 </div>
                 <div class="caro-item">
-                    <img src="chicago.jpg" alt="Chicago" class="d-block" style="width:100%">
+                <div class="card-item flex-column">
+                        <h3>Thẻ Ghi Nhớ</h3>
+                        <img src="{{ asset('imgs/partone-1.avif') }}" alt="Los Angeles" class="d-block img-card" style="width:100%">
+                    </div>
                 </div>
                 <div class="caro-item">
-                    <img src="ny.jpg" alt="New York" class="d-block" style="width:100%">
+                <div class="card-item flex-column">
+                        <h3>Kiểm Tra</h3>
+                        <img src="{{ asset('imgs/partone-2.avif') }}" alt="Los Angeles" class="d-block img-card" style="width:100%">
+                    </div>
+                </div>
+                <div class="caro-item active">
+                    <div class="card-item flex-column">
+                        <h3>Ghép Thẻ</h3>
+                        <img src="{{ asset('imgs/partone-3.avif') }}" alt="Los Angeles" class="d-block img-card" style="width:100%">
+                    </div>
+                </div>
+                <div class="caro-item active">
+                    <div class="card-item flex-column">
+                        <h3>Lời Giải Chuyên Gia</h3>
+                        <img src="{{ asset('imgs/partone-4.avif') }}" alt="Los Angeles" class="d-block img-card" style="width:100%">
+                    </div>
                 </div>
                 </div>
+                
 
                 <!-- Left and right controls/icons -->
-                <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                <!-- <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon"></span>
                 </button>
                 <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
                 <span class="carousel-control-next-icon"></span>
                 </button>
-                </div>
+                </div> -->
             <!-- ---------- -->
             </div>
+            <!-- ---------PART TWO---------- -->
+             <div class="container content-part--two flex-row">
+              <div class="img-part">
+                <img class="" src="{[{ asset('imgs/parttwo.avif') }]}" alt="">
+              </div>
+              <div class="text-part">
+                <h3>Mọi lớp học, mọi bài thi, một ứng dụng học tập tối ưu. </h3>
+                <p>Tạo thẻ ghi nhớ của riêng bạn hoặc tìm học phần do giáo viên, học sinh và chuyên gia tạo nên. Học mọi lúc, mọi nơi với ứng dụng miễn phí của chúng tôi.</p>
+              </div>
+             </div>
+             <!-- ---------PART THREE---------- -->
+             <div class="container content-part--three flex-row-reverse">
+              <div class="img-part">
+                <img class="" src="{[{ asset('imgs/partthree.avif') }]}" alt="">
+              </div>
+              <div class="text-part">
+                <h3>Chọn cách học mà bạn muốn. </h3>
+                <p>Biến thẻ ghi nhớ thành câu hỏi trắc nghiệm và nhiều nội dung khác với chế độ Học. Củng cố kiến thức của bạn bằng các trò chơi học tập như Ghép thẻ.</p>
+              </div>
+             </div>
+             <!-- ---------PART FOUR---------- -->
+             <div class="container content-part--four flex-row">
+              <div class="img-part">
+                <img class="" src="{[{ asset('imgs/partfour.avif') }]}" alt="">
+              </div>
+              <div class="text-part">
+                <h3>Chuẩn bị ôn thi cho bất kỳ môn học nào. </h3>
+                <p>Ghi nhớ mọi thứ với các bài kiểm tra thử và phiên học được cá nhân hóa. 98% học sinh cho biết Flashcard đã giúp họ hiểu bài hơn.</p>
+              </div>
+             </div>
+             <!-- ---------PART FIVE---------- -->
+             <div class="container content-part--five flex-row-reverse">
+              <div class="img-part">
+                <img class="" src="{[{ asset('imgs/partfive.avif') }]}" alt="">
+              </div>
+              <div class="text-part">
+                <h3>Truyền năng lượng cho học sinh. </h3>
+                <p>Giúp mọi học viên tự tin học bất cứ điều gì. Với các học phần miễn phí, chế độ học và trò chơi trong lớp như Flashcard Live, bạn có thể ngay lập tức tạo ra một lớp học gắn kết hơn.</p>
+              </div>
+             </div>
+
+
+             <!-- -------------end main--------------- -->
            </div>
 
 
@@ -171,7 +242,7 @@
             <div
               class="footer_box footer_box--2 gird--center gird--center--column"
             >
-              <p class="text" style="color: white">
+              <p class="text" >
                 Đăng ký để nhận thông tin cập nhật mới nhất.
               </p>
               <div class="email_button gird gird--center gird--center--row">
